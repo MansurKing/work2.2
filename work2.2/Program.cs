787a8375520d36fd8773fab78eb7ff1b4b5effbf
MansurKing <102825225+MansurKing@users.noreply.github.com>
2022-04-06 17:50:50 +0300

    init

---
+++ work2.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace work2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfRepetitions;
+            string message;
+
+            Console.WriteLine("Данная программа осуществляет цикл сообщений от 1 до n-числа.");
+            Console.Write($"Введите n-число повторений: ");
+            numberOfRepetitions = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= numberOfRepetitions; i++)
+            {
+                Console.WriteLine(i);
+                Console.WriteLine("Для продолжения цикла нажмите клавишу 'Enter'");
+                Console.WriteLine("Для завершения цикла введите слово 'exit'");
+                message = Console.ReadLine();
+                if (message == "exit")
+                {
+                    Console.WriteLine($"Программа завершилась на {i} числе из {numberOfRepetitions} возможных.");
+                    break;
+                }
+            }
+        }
+    }
+}
